bdf4bf3b475334bd50149780eef1c38cc27c44f0
Daniel Awaritefe <Daniel.Awaritefe@biglight-id-600.home>
2024-02-04 16:46:31 +0000

    console logging name of habitible planets found

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const isHabitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 fs.createReadStream("kepler_data.csv")
   .pipe(
@@ -11,12 +20,18 @@ fs.createReadStream("kepler_data.csv")
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      isHabitablePlanets.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(results);
-    console.log("Done!");
+    console.log(
+      isHabitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`${isHabitablePlanets.length} habitible planets found!`);
   });
